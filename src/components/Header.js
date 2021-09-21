737101c42fbae20e09449387ea008037d92be53d
pasDamola <oyindadavid18@gmail.com>
2021-09-21 15:40:22 +0100

    added material close button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from "styled-components"
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 function Header() {
     return (
@@ -19,8 +20,20 @@ function Header() {
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
+                <CustomMenu/>
             </RightMenu>
-            <CustomMenu/>
+            <BurgerNav>
+                <CloseWrapper >
+                    <CustomClose />
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">CyberTruck</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Existing Inventory</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -32,10 +45,12 @@ const Container = styled.div`
     position: fixed;
     display: flex;
     align-items: center;
+    justify-content: space-between;
     padding: 0 20px;
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 
 
@@ -51,6 +66,10 @@ const Menu = styled.div`
         padding: 0 10px;
         flex-wrap: nowrap;
     }
+
+    @media(max-width: 768px) {
+        display: none;
+    }
 `
 const RightMenu = styled.div`
     display: flex;
@@ -65,4 +84,36 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  li {
+      padding: 15px 0;
+      border-bottom: 1px solid rgba(0, 0, 0, .2);
+  }
+
+  a {
+      font-weight: 600;
+  }
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
 `
